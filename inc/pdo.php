e13744a04d41ecd3067a376d44d908859161f432
ModjoInc <habibem@gmail.com>
2017-08-30 13:03:12 +0200

    maj directories

--- inc/pdo.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	try {
-		$db = new PDO('mysql:host=localhost;dbname=mini_chat;charset=utf8', 'root', 'mvdtvw28', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-	} catch (Exception $e){
-		die('Error: '.$e->getMessage());
-	}
